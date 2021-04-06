86c2b76bc174956244b939fe183e634984c55004
ayaanmondal <ayanmondal9929@gmail.com>
2021-04-06 20:09:50 +0530

    React Router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
-import React, { Fragment } from 'react';
-import './App.css';
-import { Navbar } from './components/layout/Navbar';
-import { Landing } from './components/layout/Landing';
+import React, { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navbar } from "./components/layout/Navbar";
+import { Landing } from "./components/layout/Landing";
+import { Register } from "./components/auth/Register";
+import { Login } from "./components/auth/Login";
 
 const App = () => (
-  <Fragment>
-    <Navbar/>
-    <Landing/>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/">
+        <Landing />
+      </Route>
+      <section className="container">
+        <Switch>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
-
-
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom"
 
 export const Navbar = () => {
-    return (
-        <nav className="navbar bg-dark">
-        <h1>
-          <a href="index.html"><i className="fas fa-code"></i> BUIE Blogs v2</a>
-        </h1>
-        <ul>
-          <li><a href="profiles.html">Community</a></li>
-          <li><a href="register.html">Register</a></li>
-          <li><a href="login.html">Login</a></li>
-        </ul>
-      </nav>
-    )
-}
-
-
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/">
+          <i className="fas fa-code"></i> BUIE Blogs v2
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/profile">Community</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
